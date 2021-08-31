af4dcdc210a074d34a7270e8da870646a7d44d15
SGCMarkus <markusornik@gmail.com>
2021-08-31 16:17:00 +0200

    berlin: add and copy hotword permissions

--- device.mk
+++ device.mk
@@ -226,6 +226,10 @@ PRODUCT_PACKAGES += \
     libhidltransport.vendor \
     libhwbinder.vendor
 
+# HotwordEnrollement app permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
